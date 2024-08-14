945ad82d552c7349bcbe9de5efda3bb1a367d2fc
BulahanJoren <145977667+Zoowen21@users.noreply.github.com>
2024-08-14 21:35:31 +0800

    Create MovieQuotesInfor.java

---
+++ Chapter 1/MovieQuotesInfor.java
@@ -0,0 +1,8 @@
+public class MovieQuoteInfor {
+   public static void main(String args[]) {
+      System.out.println("I Would Have Really Liked Just Doing Laundry And Taxes With You.");
+      System.out.println("Everything Everywhere All at Once");
+      System.out.println("2022");
+       System.out.println("starring Michelle Yeoh and Ke Huy Quan");
+   }
+}
