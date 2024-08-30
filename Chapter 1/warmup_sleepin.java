c0f190fc3c8dd1db0c8b2c6d56c7b0d9f3110ba5
joren-bulahan <145977667+joren-bulahan@users.noreply.github.com>
2024-08-30 08:58:00 +0700

    Update warmup_sleepin.java

--- Chapter 1/warmup_sleepin.java
+++ Chapter 1/warmup_sleepin.java
@@ -1,11 +1,22 @@
-public class warmup_sleepin{
-   public boolean Alert(boolean weekday, boolean vacation){
-      return !weekday || vacation;
-   }
-   public static void main(String[] args){
-      warmup_sleepin checker = new warmup_sleepin();
-      System.out.println("sleepIn(flase,flase) = " + checker.Alert(false,false));
-      System.out.println("sleepIn(true,false) = " + checker.Alert(true,false));
-      System.out.println("sleepIn(false,true) = " + checker.Alert(false,true));
-   }
+import java.util.Scanner;
+
+public class warmup_sleepin {
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+ return !weekday || vacation;
+    }
+    public static void main(String[] args) {
+        warmup_sleepin sleeping = new warmup_sleepin();
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("is it Weekday? (true/false)");
+        boolean weekends = scanner.nextBoolean();
+        
+        System.out.println("is it Vacation? (true/false)");
+        boolean vacations = scanner.nextBoolean();
+        
+        boolean result = sleepIn(weekends,vacations);
+        
+        System.out.println("The sleepIn result is: " + result);
+        
+    }
 }
