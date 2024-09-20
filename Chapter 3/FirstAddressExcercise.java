c5224eb627c19759fdfece0927336b6ba249117d
joren-bulahan <145977667+joren-bulahan@users.noreply.github.com>
2024-09-20 09:12:42 +0700

    Create FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,11 @@
+public class getAddress {
+    public static void main(String[] args) {
+        String address = getAddress();
+        System.out.println("First Java application");
+        System.out.println(address);
+    }
+
+    public static String getAddress() {
+        return "Company: Apple\n1 Infinite Loop\nCupertino, CA 95014";
+    }
+}
