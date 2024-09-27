281d9b29f6a4fb0205fd78b096e44367b94676f8
joren-bulahan <145977667+joren-bulahan@users.noreply.github.com>
2024-09-27 09:00:25 +0700

    TestCircle.java

--- Chapter 4/TestCircle.java
+++ Chapter 4/TestCircle.java
@@ -1,1 +1,20 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle a = new Circle();
+        a.setRadius(3);
+        System.out.println("Radius: " + a.getRadius());
+        System.out.println("Diameter: " + a.getDiameter());
+        System.out.println("Area: " + a.getArea());
 
+        Circle b = new Circle();
+        b.setRadius(20);
+        System.out.println("Radius: " + b.getRadius());
+        System.out.println("Diameter: " + b.getDiameter());
+        System.out.println("Area: " + b.getArea());
+
+        Circle c = new Circle();
+        System.out.println("Radius: " + c.getRadius());
+        System.out.println("Diameter: " + c.getDiameter());
+        System.out.println("Area: " + c.getArea());
+    }
+}
